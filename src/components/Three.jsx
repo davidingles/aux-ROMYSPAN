1b90e4bdf37f93a21b96caf591de879d7cd8d8ea
davidingles <davidingleslopez@gmail.com>
2024-03-12 13:03:55 +0100

    primer commit2

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -15,7 +15,7 @@ const Models = [
   // { title: 'antorcha2', url: './antorcha2.glb', miEscala: .5, miPosicion: -0.3 },
   // { title: 'antorcha1', url: './antorcha1.glb', miEscala: .5, miPosicion: -0.3 },
   // { title: 'donaciones', url: './donaciones.glb', miEscala: .2, miPosicion: -0.3 },
-  { title: 'GARLAND', url: './garland.glb', miEscala: 1, miPosicion: -0.02 },
+  { title: 'ROMYSPAN', url: './romyspan.glb', miEscala: 1, miPosicion: -0.02 },
 ]
 
 function Model({ url, miEscala, miPosicion }) {
